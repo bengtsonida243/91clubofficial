b0ef23a9d3cd56bdf94ac8f11b8d8596e2e2eabb
Ramuk9987 <jasonkumar9987@gmail.com>
2024-06-20 19:56:48 +0800

    adding content

--- src/Content.js
+++ src/Content.js
@@ -4,6 +4,9 @@ import Image1 from "./image/91_Club_5D_Lotre_Number_Prediction_Game.webp";
 import Image2 from "./image/91_Club_K3_Lotre_Number_Prediction_Game.webp";
 import Image3 from "./image/91_Club_TRX_Hash_Colour_Prediction_Game.webp";
 import Image4 from "./image/91_Club_Wingo_Colour_Prediction_Game.webp";
+import Image5 from "./image/91_Club_Question.webp";
+import Image6 from "./image/game-9-svgrepo-com.svg";
+import Image7 from "./image/91Club-Official-Banner.webp";
 import "./Content.css";
 
 import History from "./History";
@@ -30,11 +33,30 @@ const Content = () => {
     <div className="container">
       <div className="row">
         <img src={Icon} alt="icon" className="img-fluid icon-style d-flex mx-auto mt-3" />
-        <h1 className="text-center">91 Club Official</h1>
+        <h1 className="text-center">Welcome to 91 Club Official</h1>
+      </div>
+      <div className="row d-flex my-5">
+        <div className="col my-auto">
+          <h2 className="red">What is 91 Club?</h2>
+          <p>Welcome to 91 Club Official, where gaming is more than just a pastime—it's a lifestyle. 91 Club Official is an exciting online platform dedicated to the dynamic world of color prediction games, which have quickly become a popular activity among gaming enthusiasts in India. Our platform acts as a digital bridge between tradition and the future, providing a distinct blend of historic gaming pleasure and the seamless convenience of modern technology.</p>
+        </div>
+        <div className="col">
+          <img  src={Image5} className="img-fluid" alt="91 Club | Questions" />
+        </div>
+      </div>
+      <div className="row d-flex my-5">
+      <img  src={Image6} className="img-fluid img-icon" alt="91 Club | Game" />
+        <h2 className="white text-center">91 CLUB TOP GAMES</h2>
+        <div className="col my-auto">
+          <h3 className="white">Colour Prediction Game</h3>
+          <p>In this simple but entertaining game, try your luck by predicting the color sequence. The Color Prediction Game boasts vivid and appealing designs, as well as multiple ways to win, making it a popular choice for gamers looking for both pleasure and quick success.</p>
+        </div>
+        <div className="col">
+          <img  src={Image7} className="img-fluid img-banner" alt="91 Club | Banner" />
+        </div>
       </div>
       <div className="row">
             {images.map((image) => (
-            
                 <div className="col-lg-6 mb-4">
                     <img key={image.id} src={image.src} className="img-fluid img-game" alt={image.alt} />
                 </div>
@@ -57,7 +79,6 @@ const Content = () => {
       <footer className="d-flex justify-content-center mt-5">
         <p className="text-light">91 Club Official | All Rights Reserved.</p>
       </footer>
-      
     </div>
   );
 }
